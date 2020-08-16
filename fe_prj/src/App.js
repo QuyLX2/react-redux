5a4577aa6a1a5f6899034496e2564a6589a05fb8
quylx2 <lexuanquy4198@gmail.com>
2020-08-16 21:12:10 +0700

    first commit

---
+++ fe_prj/src/App.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import UserHomePageDefault from './views/pages/UserHomePageDefault';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import LoginPage from './views/pages/LoginPage';
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div>
+          <Switch>
+            <Route exact path="/">
+              <UserHomePageDefault />
+            </Route>
+            <Route  path="/login">
+              <LoginPage />
+            </Route>
+            
+          </Switch>
+        </div>
+      </Router>
+    );
+  }
+}
+
+export default App;
