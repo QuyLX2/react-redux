3b19522efefa3e592a2d40a3657f5afbece88892
duongdat98 <duongdat@coders.com>
2020-08-17 11:59:15 +0700

    hjx

---
+++ fe_prj/src/routes.js
@@ -0,0 +1,144 @@
+import React from 'react';
+import { UserOutlined } from '@ant-design/icons';
+
+
+const routes = [
+    {
+        path: "/overview",
+        name: "Overview",
+        icon: <UserOutlined />,
+        component: <div>Overview Course</div>,
+      },
+    {
+        path: '',
+        name: 'Lecture 1',
+        icon: <UserOutlined />,
+        children: [
+            {
+                path: '/lecture-1/lesson-1',
+                name: 'Lesson 1',
+                component: <div>Content</div>,
+            },
+            {
+                path: '/lecture-1/lesson-2',
+                name: 'Lesson 2',
+                component: <div>Content</div>
+            },
+            {
+                path: '/lecture-1/lesson-3',
+                name: 'Lesson 3',
+                component: <div>Content</div>
+            },
+            {
+                path: '/lecture-1/lesson-4',
+                name: 'Lesson 4',
+                component: <div>Content</div>
+            },
+            {
+                path: '/lecture-1/lesson-5',
+                name: 'Lesson 5',
+                component: <div>Content</div>
+            },
+        ]
+    },
+    {
+        path: "/lecture-2",
+        name: 'Lecture 2',
+        icon: <UserOutlined />,
+        children: [
+            {
+                path: '/lecture-2/lesson-1',
+                name: 'Lesson 1',
+                component: <div>Content</div>
+            },
+            {
+                path: '/lecture-2/lesson-2',
+                name: 'Lesson 2',
+                component: <div>Content</div>
+            },
+            {
+                path: '/lecture-2/lesson-3',
+                name: 'Lesson 3',
+                component: <div>Content</div>
+            },
+            {
+                path: '/lecture-2/lesson-4',
+                name: 'Lesson 4',
+                component: <div>Content</div>
+            },
+            {
+                path: '/lecture-2/lesson-5',
+                name: 'Lesson 5',
+                component: <div>Content</div>
+            },
+        ]
+    },
+    {
+        path: '/lecture-3',
+        name: "Lecture 3",
+        icon: <UserOutlined />,
+        children: [
+            {
+                path: '/lecture-3/lesson-1',
+                name: 'Lesson 1',
+                component: <div>Content</div>
+            },
+            {
+                path: '/lecture-3/lesson-2',
+                name: 'Lesson 2',
+                component: <div>Content</div>
+            },
+            {
+                path: '/lecture-3/lesson-3',
+                name: 'Lesson 3',
+                component: <div>Content</div>
+            },
+            {
+                path: '/lecture-3/lesson-4',
+                name: 'Lesson 4',
+                component: <div>Content</div>
+            },
+            {
+                path: '/lecture-3/lesson-5',
+                name: 'Lesson 5',
+                component: <div>Content</div>
+            },
+        ]
+    },
+    {
+        path: '/final-exam',
+        name: "Final Exam",
+        icon: <UserOutlined />,
+        children: [
+            {
+                path: "/final-exam/exam-1",
+                name: "Exam 1",
+                component: <div>Content</div>
+            },
+            {
+                path: "/final-exam/exam-2",
+                name: "Exam 2",
+                component: <div>Content</div>
+            },
+            {
+                path: "/final-exam/exam-3",
+                name: "Exam 3",
+                component: <div> Content </div>
+            },
+            {
+                path: "/final-exam/exam-4",
+                name: "Exam 4",
+                component: <div>Content</div>
+            },
+            {
+                path: "/final-exam/exam-5",
+                name: "Exam 5",
+                component: <div>Content</div>
+            },
+        ]
+    }
+
+]
+
+
+export default routes;
--- fe_prj/src/views/components/sideBar/NavLink.js
+++ fe_prj/src/views/components/sideBar/NavLink.js
@@ -1,32 +1,12 @@
 import React, { Component } from 'react';
-import { Layout, Menu, message } from 'antd';
-import {
-    UserOutlined,
-    LaptopOutlined,
-    NotificationOutlined,
-} from '@ant-design/icons';
+import { Layout, Menu } from 'antd';
+import { Link } from 'react-router-dom';
+import routes from '../../../routes';
 
 
 const { SubMenu } = Menu;
 const { Sider } = Layout;
-const menu = (
-    <Menu onClick={handleMenuClick}>
-        <Menu.Item key="1" icon={<UserOutlined />}>
-            1st menu item
-      </Menu.Item>
-        <Menu.Item key="2" icon={<UserOutlined />}>
-            2nd menu item
-      </Menu.Item>
-        <Menu.Item key="3" icon={<UserOutlined />}>
-            3rd menu item
-      </Menu.Item>
-    </Menu>
-);
 
-function handleMenuClick(e) {
-    message.info('Click on menu item.');
-    console.log('click', e);
-}
 
 class NavLink extends Component {
     render() {
@@ -38,48 +18,20 @@ class NavLink extends Component {
                     defaultOpenKeys={['sub1']}
                     style={{ height: '100%' }}
                 >
-                    <SubMenu
-                        key="sub1"
-                        title={
-                            <span>
-                                <UserOutlined />
-                            subnav 1
-                            </span>
-                        }
-                    >
-                        <Menu.Item key="1">option1</Menu.Item>
-                        <Menu.Item key="2">option2</Menu.Item>
-                        <Menu.Item key="3">option3</Menu.Item>
-                        <Menu.Item key="4">option4</Menu.Item>
-                    </SubMenu>
-                    <SubMenu
-                        key="sub2"
-                        title={
-                            <span>
-                                <LaptopOutlined />
-                            subnav 2
-                            </span>
-                        }
-                    >
-                        <Menu.Item key="5">option5</Menu.Item>
-                        <Menu.Item key="6">option6</Menu.Item>
-                        <Menu.Item key="7">option7</Menu.Item>
-                        <Menu.Item key="8">option8</Menu.Item>
-                    </SubMenu>
-                    <SubMenu
-                        key="sub3"
-                        title={
-                            <span>
-                                <NotificationOutlined />
-                            subnav 3
-                            </span>
-                        }
-                    >
-                        <Menu.Item key="9">option9</Menu.Item>
-                        <Menu.Item key="10">option10</Menu.Item>
-                        <Menu.Item key="11">option11</Menu.Item>
-                        <Menu.Item key="12">option12</Menu.Item>
-                    </SubMenu>
+                    {routes.map((route, index) => {
+                        return route.children ? (
+                                <SubMenu key={index} icon={route.icon} title={route.name}>
+                                    {route.children.map((child, idx) => (
+                                        <Menu.Item key={child.name + idx}><Link to={child.path}>{child.name}</Link></Menu.Item>
+                                    ))}
+                                </SubMenu>
+                            ) : (
+                                <Menu.Item key={index} icon={route.icon}>
+                                    {console.log('path', route.path)}
+                                    <Link to={route.path}>{route.name}</Link>
+                                </Menu.Item>
+                            )
+                        })}
                 </Menu>
             </Sider>
         )
